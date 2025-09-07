adc029bf240253eecb7732d175e6824e5e132f4f
madbull12 <huangandrian02@gmail.com>
2025-09-07 23:41:21 +0700

    choir: added cursor click icon animation and dropdownmenu component

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,9 +1,11 @@
 import Logo from "@/src/app/components/logo"
+import { CursorClickIcon } from "@/src/app/components/ui/cursor-click"
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between h-16 px-4 bg-white shadow-md sticky top-0 z-50">
         <Logo />
+        <CursorClickIcon size={24} />
     </nav>
   )
 }
